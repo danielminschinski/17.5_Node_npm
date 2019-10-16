5f9f968da8d8c50b9af1568df9e549299bf56fe6
Daniel Minschinski <danielminschinski@gmail.com>
2019-10-16 21:41:08 +0200

    Update OSinfo

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var colors = require('colors');
 var calcTime = require('../modules/calcTime');
 
 function getOSinfo(){
@@ -12,11 +13,11 @@ function getOSinfo(){
     var cpu = os.cpus()[0].model;
     var uptime = calcTime.print();
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime:', uptime);
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+    console.log('System:'.green, type);
+    console.log('Release:'.underline.red, release);
+    console.log('CPU model:'.inverse, cpu);
+    console.log('Uptime:'.rainbow, uptime);
+    console.log('User name:'.zebra, userInfo.username);
+    console.log('Home dir:'.yellow, userInfo.homedir);
 }
 exports.print = getOSinfo;
